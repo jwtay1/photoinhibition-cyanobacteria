e1dbb20be0ce8e3bd08b0158aaad822a5ec521f5
Jian Tay <jian.tay@colorado.edu>
2020-12-18 19:46:47 -0700

    Added code to plot cell lengths and intensities

--- analysis/loaddataphotoinhibition.m
+++ analysis/loaddataphotoinhibition.m
@@ -10,6 +10,7 @@ S17 = PhotodamageAnalyzer;
 S17 = importdata(S17, 'D:\Projects\2020Feb Photodamage\data\MergeProcess20200429\Reprocessed\ChannelRed,Cy5,RFP_Seq0000_series17_merged.mat');
 S17.iXY = 17;
 
+%%
 S20 = PhotodamageAnalyzer;
 S20 = importdata(S20, 'D:\Projects\2020Feb Photodamage\data\MergeProcess20200429\ChannelRed,Cy5,RFP_Seq0000_series20_merged.mat');
 S20 = setFileMetadata(S20, 'Filename', 'D:\Projects\2020Feb Photodamage\data\20200222\ChannelRed,Cy5,RFP_Seq0000.nd2; D:\Projects\2020Feb Photodamage\data\20200222\ChannelRed,Cy5,RFP_Seq0001.nd2');
